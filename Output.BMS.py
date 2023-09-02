7c7bc3be45a99ceb555ef427ab2dc65134f17d8d
mohan-81 <127953745+mohan-81@users.noreply.github.com>
2023-09-02 14:08:09 +0530

    Add files via upload

---
+++ Output.BMS.py
@@ -0,0 +1,127 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\HP\Documents\PROJECTS.moh\BANK-MANAGEMENT-SYSTEM(.py)\Bank.py (code)
+				**********************
+				BANK MANAGEMENT SYSTEM
+				**********************
+				Brought To You By:
+				Bank of Bharat
+
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+1
+Enter the account no : 101
+Enter the account holder name : Thambabattula mohan
+Ente the type of account [C/S] : s
+Enter The Initial amount(>=500 for Saving and >=1000 for current700
+
+
+
+Account Created
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+2
+	Enter The account No. : 101
+Enter the amount to deposit : 2500
+Your account is updted
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+4
+	Enter The account No. : 101
+Your account Balance is =  3200
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+3
+	Enter The account No. : 101
+Enter the amount to withdraw : 300
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+5
+101   Thambabattula mohan   s   2900
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+7
+	Enter The account No. : 101
+Enter the account holder name : michael
+Enter the account Type : c
+Enter the Amount : 5000
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+5
+101   michael   c   5000
+Enter your choice : 
+	MAIN MENU
+	1. NEW ACCOUNT
+	2. DEPOSIT AMOUNT
+	3. WITHDRAW AMOUNT
+	4. BALANCE ENQUIRY
+	5. ALL ACCOUNT HOLDER LIST
+	6. CLOSE AN ACCOUNT
+	7. MODIFY AN ACCOUNT
+	8. EXIT
+	Select Your Option (1-8) 
+8
+	Thanks for using bank managemnt system
